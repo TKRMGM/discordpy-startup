23f4b777af768c0b09761251aa318de628c11fd9
TKRMGM <66470753+TKRMGM@users.noreply.github.com>
2020-06-06 23:10:26 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,12 +14,12 @@ async def greeting_gm():
     channel = client.get_channel('718811732243382345')
     while True:
         print(datetime.datetime.now().minute)
-        if(datetime.datetime.now().minute==4):
+        if(datetime.datetime.now().minute==10):
             print("eeee")
-            await client.send_message(channel, 'おはよう' + str(datetime.datetime.now()))
+            await channel.send('おはよう' + str(datetime.datetime.now()))
             await asyncio.sleep(1)
-        elif(datetime.datetime.now().minute==5):
-            await client.send_message(channel, 'gtegegeegう' + str(datetime.datetime.now()))
+        elif(datetime.datetime.now().minute==11):
+            await channel.send('gtegegeegう' + str(datetime.datetime.now()))
             await asyncio.sleep(1)
         else:
             await asyncio.sleep(1)
